07668af148eb5b75d6acac280da4f9c07a55211f
yao <6466617513@qq.com>
2017-12-26 15:36:47 +0800

    mk a mobile init

---
+++ mobile-init/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="app">
+        <router-view/>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'app'
+    }
+</script>
+
+<style>
+    @import './styles/init.css';
+    #app {
+        max-width: 750px;
+        margin: 0 auto;
+    }
+</style>
---
+++ mobile-init/src/views/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        这是基于网易移动端适配方案的初始化项目,按照iPhone6设计，使用时，将设计稿的像素除100 换算成rem即可
+    </div>
+</template>
+
+<script>
+    export default {
+        name: '',
+        data() {
+            return {}
+        }
+    }
+</script>
+
+
+<style scoped>
+div {
+    font-size: 24px;
+}
+
+</style>
